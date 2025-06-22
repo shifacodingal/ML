08022a5e9c0ef630115827b0dca240ac392163ee
shifacodingal <shifacodingal@gmail.com>
2025-06-22 20:57:45 +0530

    Add files via upload

---
+++ Welcome.py
@@ -0,0 +1,10 @@
+#print a sentence
+print("Welcome to the world of programming ! ")
+#print a number
+print(5)
+#next line after print
+print("Hello Python \n")
+#pass multiple arguments for printing
+print("Hello", 5)
+#End argument of print statement
+print("Welcome to ",end="*")
